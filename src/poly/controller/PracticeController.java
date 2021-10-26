41d4bf3c11180aeef293557585eb4dc5f9537f44
DESKTOP-G5HRTTN\user <ohhanmin000@gmail.com>
2021-10-26 14:45:48 +0900

    오한민 수정 2

--- src/poly/controller/PracticeController.java
+++ src/poly/controller/PracticeController.java
@@ -1,8 +1,14 @@
 package poly.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PracticeController {
-	
+	@RequestMapping(value="sid", method=RequestMethod.GET)
+	public String sid() {
+		return "/sid";
+	}
 }
