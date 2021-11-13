beee1a6da890f472a5e1ab825b006dd2f481d4a0
Gros19 <kjj6393@gmail.com>
2021-11-13 11:01:50 +0900

    com

---
+++ src/poly/controller/PracticeController.java
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+package poly.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PracticeController {
+	 public String practice() {
+		 return "dd";
+	 }
+=======
+package poly.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PracticeController {
+	 public String practice() {
+		 return "dd";
+	 }
+>>>>>>> origin/practice
+}
